94d054858ddd4592dae6a88fc05afec64578cfb2
Vinicius <vcsousa414@gmail.com>
2023-11-14 22:27:18 -0300

    Aula - Estrutura visual da lista de horários

---
+++ src/Components/Calendar/index.tsx
@@ -0,0 +1,60 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarAction,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+import { getWeekDays } from '@/src/utils/get-week-days'
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Dezembro <span>2022</span>
+        </CalendarTitle>
+
+        <CalendarAction>
+          <button>
+            <CaretRight />
+          </button>
+          <button>
+            <CaretLeft />
+          </button>
+        </CalendarAction>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '@/src/Components/Calendar'
+import { Container } from './styles'
+
+export function CalendarStep() {
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  )
+}
